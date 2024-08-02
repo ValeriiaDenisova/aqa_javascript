4e6ed5826b740ef84d0f2cb6895c5dfa75d0d90f
denisova <v.denisova@comparus.de>
2024-08-02 15:37:07 +0300

    Homework 3. Imported chalk

---
+++ classwork/test1.js
@@ -0,0 +1,41 @@
+//console.log("hello")
+const A = "Valeriia"
+const B = 'Hello 2'
+const C = `Hello ${A}`
+//console.log(C)
+
+const val1 = 42
+const val2 = 42.42
+//console.log(1/0)
+
+//console.log("number"/2)
+// console.log(isNaN(42))
+// console.log(isNaN("42"))
+// console.log(isNaN("42 j"))
+// console.log(Number.isInteger("42"))
+//
+// console.log(11e4)
+
+// undefined
+let a
+console.log(a)
+
+function doNothing() {
+}
+let res = doNothing()
+console.log(res)
+
+let b
+if (b === undefined) {
+    console.log("b is undefined")
+}
+
+let f = null
+let g
+console.log(f == g)
+console.log(f === g)
+
+let n = Symbol()
+let m = Symbol("fkgdkgjfdg")
+console.log(n)
+console.log(m)
---
+++ homework3/nodeBasics/index.mjs
@@ -0,0 +1,3 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('In a world where chickens wear bow ties and dance the cha-cha, the only thing more surprising is a cat that sings opera every Tuesday.'));
