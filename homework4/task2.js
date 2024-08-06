42acfc668e83f3d7189f5d0961d39e143e9513da
denisova <v.denisova@comparus.de>
2024-08-06 19:34:11 +0300

    Homework 4. JavaScript primitive data types

---
+++ homework4/task2.js
@@ -0,0 +1,8 @@
+let name1 = "Valeria"
+let name2 = "Maksym"
+
+let result1 = "Hi " + name1 + " and " + name2
+console.log(result1)
+
+let result2 = `Hello ${name1} and ${name2}`
+console.log(result2)
