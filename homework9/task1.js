372a827ead74a6337a2275b04286d647aa7ee404
ValeriiaDenisova <dmytrijevavi@gmail.com>
2024-09-04 21:05:21 +0300

    ДЗ 9.1. Objects in JavaScript

---
+++ homework9/task1.js
@@ -0,0 +1,8 @@
+const student = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    courses: ["Математика", "Історія", "Програмування"]
+};
+
+console.log(student)
---
+++ homework9/task2.js
@@ -0,0 +1,10 @@
+let book = {
+    title: "Пешеход",
+    author: "Рэй Брэдбери",
+    year: 1951
+};
+
+let { title, author } = book;
+
+console.log(title);
+console.log(author);
